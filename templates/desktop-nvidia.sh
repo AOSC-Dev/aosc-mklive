03e0e52253c66023a5bea2a1af5085a91906ad42
Cyan <cyan@cyano.uk>
2024-07-01 21:34:47 +0800

    installer: allow script-only templates
    - This has to be done for a trick where an initramfs image with NVIDIA
      driver preloaded can be generated for LiveKit.

---
+++ templates/desktop-nvidia.sh
@@ -0,0 +1,13 @@
+echo "Installing plymouth-livekit ..."
+systemd-nspawn -D $TGT -- oma --no-check-dbus install --yes plymouth-livekit
+
+echo "Customising Plymouth theme ..."
+sed -e 's|semaphore|livekit|g' \
+    -i $TGT/etc/plymouth/plymouthd.conf
+
+echo "Generating a LiveKit initramfs image with NVIDIA driver ..."
+cp -a ${TOP}/dracut/90aosc-livekit-loader \
+	$TGT/usr/lib/dracut/modules.d/
+systemd-nspawn -D $TGT -- dracut /live-initramfs-nvidia.img --add "aosc-livekit-loader" --add-drivers "nvidia nvidia-modeset nvidia-uvm nvidia-drm"
+install -Dvm644 $TGT/live-initramfs-nvidia.img $OUTDIR/boot/live-initramfs-nvidia.img
+rm -v $TGT/live-initramfs-nvidia.img
