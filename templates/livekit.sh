87995404e4ed887c802c8c8010891af0ecb3e0c1
Cyan <cyan@cyano.uk>
2024-07-08 14:40:43 +0800

    templates/livekit: reinstall GStreamer to workaround a strange error
    GStreamer refused to load modules unless GStreamer itself is
    reinstalled. Refreshing ld.so cache does not work.

--- templates/livekit.sh
+++ templates/livekit.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# FIXME GStreamer refused to load its modules because a bunch of symbol
+# lookup errors, even their dependencies and shared symbols does exist.
+# Reinstalling GStreamer does fix this problem, so we do it here.
+echo "Reinstalling gstreamer ..."
+pushd $TGT
+apt update
+apt download gstreamer
+systemd-nspawn -D $TGT apt install --reinstall /$(basename $(ls $TGT/gstreamer_*.deb))
+rm -r $TGT/var/cache/apt/archives/*
+rm $TGT/usr/lib/gstreamer-1.0/libgstvaapi.so
+popd
+
 echo "Copying kernel and initramfs ..."
 mkdir -p "$OUTDIR"/boot
 cp "$WORKDIR"/livekit/kernel "$OUTDIR"/boot/
