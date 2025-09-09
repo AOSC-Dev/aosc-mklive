8e1496a2a0e4cd3f6ab067f4d9f70f99d7064a77
Xinhui Yang <cyan@cyano.uk>
2025-09-09 13:27:55 +0800

    livekit: support multiple kernel configurations
    We are now shipping multiple kernel configurations for the mainline
    kernel, so there will be more than one kernel and initramfs images in
    our live media.

--- templates/desktop-nvidia.sh
+++ templates/desktop-nvidia.sh
@@ -1,3 +1,9 @@
+default_config() {
+	# Stub - loongarch64 does not have NVIDIA drivers, and there's no
+	# other kernel configurations for arm64.
+	echo ""
+}
+
 echo "Installing plymouth-livekit ..."
 systemd-nspawn -D $TGT -- \
 	oma --no-check-dbus install \
@@ -10,14 +16,36 @@ sed -e 's|semaphore|livekit|g' \
 echo "Generating a LiveKit initramfs image with NVIDIA driver ..."
 cp -a ${TOP}/dracut/90aosc-livekit-loader \
 	$TGT/usr/lib/dracut/modules.d/
-systemd-nspawn -D $TGT -- dracut /live-initramfs-nvidia.img \
+IFS=$'\n'
+KERNELS=($(ls $TGT/usr/lib/modules/))
+unset IFS
+for kernel in "${KERNELS[@]}" ; do
+	IFS='-'
+	# NOTE: do NOT add quotes around this, otherwise the version string
+	# won't be sliced.
+	components=($kernel)
+	kernel_configuration="${components[3]}"
+	if [ -n "$kernel_configuration" ] ; then
+		if [ "$(default_configuration)" = "$kernel_configuration" ] ; then
+			kernel_selected="$kernel"
+		fi
+	else
+		kernel_selected="$kernel"
+	fi
+done
+if [ -z "$kernel_selected" ] ; then
+	echo "Internal error - no kernel selected to generate an initramfs image"
+	exit 1
+fi
+systemd-nspawn -D "$TGT" -- dracut /live-initramfs-nvidia.img \
 	--add "aosc-livekit-loader" \
 	--omit "crypt mdraid lvm" \
 	--no-hostonly \
 	--add-drivers "nvidia nvidia-modeset nvidia-uvm nvidia-drm" \
-	$(ls $TGT/usr/lib/modules/)
-install -Dvm644 $TGT/live-initramfs-nvidia.img $OUTDIR/boot/live-initramfs-nvidia.img
-rm -v $TGT/live-initramfs-nvidia.img
+	"$kernel_selected"
+
+install -Dvm644 "$TGT"/live-initramfs-nvidia.img "$OUTDIR"/boot/live-initramfs-nvidia.img
+rm -v "$TGT"/live-initramfs-nvidia.img
 
 echo "Cleaning up ..."
 rm -r $TGT/var/cache/apt/archives
