7da87a64873e00d14cbabdfd75e941da88425b5c
Xinhui Yang <cyan@cyano.uk>
2026-02-27 18:02:35 +0800

    chore: add loonggpu for loongarch64

--- templates/desktop-drivers.sh
+++ templates/desktop-drivers.sh
@@ -18,6 +18,10 @@ case "$CURNAME" in
 		DRACUT_ADD_DRIVERS=" zx_core zx "
 		OUT_INITRD="live-initramfs-zhaoxin.img"
 		;;
+	desktop-loonggpu)
+		DRACUT_ADD_DRIVERS=" loonggpu "
+		OUT_INITRD="live-initramfs-loonggpu.img"
+		;;
 	desktop-arise)
 		DRACUT_ADD_DRIVERS=" arise "
 		OUT_INITRD="live-initramfs-arise.img"
